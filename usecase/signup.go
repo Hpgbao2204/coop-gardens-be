b849d992b20f36660359b7c5069145ec532d8dc6
CryshUIT <bao15112229@gmail.com>
2025-03-22 17:21:45 +0700

    sign-up

--- usecase/signup.go
+++ usecase/signup.go
@@ -1,25 +1,18 @@
 package usecase
 
 import (
-	"errors"
-
+	"coop-gardens-be/internal/models"
 	"coop-gardens-be/internal/repository"
-
-	"golang.org/x/crypto/bcrypt"
 )
 
 func Signup(email, username, password string) error {
-	existingUser, _ := repository.GetUserByEmail(email)
-	if existingUser.ID != 0 {
-		return errors.New("Email already registered")
+	user := &models.User{
+		Email:    email,
+		Username: username,
+		Password: password,
 	}
 
-	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
-	if err != nil {
-		return err
-	}
-	
-	err = repository.CreateUser(email, username, string(hashedPassword))
+	err := repository.CreateUser(user)
 	if err != nil {
 		return err
 	}
